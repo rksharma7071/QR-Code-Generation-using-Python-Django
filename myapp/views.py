be11b1cfcc9060515701462107df403759773f7e
rksharma7071 <rksharma7071@gmail.com>
2023-08-08 15:53:17 +0530

    'qrcode2.1'

--- myapp/views.py
+++ myapp/views.py
@@ -1,9 +1,5 @@
 from django.shortcuts import render
 import qrcode
-import os
-import random
-import string
-# from myapp.models import Qrcode_model  
 
 def main(request):
     if request.method == "GET":
@@ -12,12 +8,7 @@ def main(request):
         url = request.POST.get('url')
         qr_code = qrcode.make(url)
 
-        # characters = string.ascii_letters + string.digits
-        # random_string = ''.join(random.choice(characters) for _ in range(10))
-        # qr_code.save(f"static/{random_string}.png")
-        # image = f'{random_string}.png'
-        
-        qr_code.save(f"static/qrcode.png")
+        qr_code.save("static/qrcode.png")
         image = 'qrcode.png'
         
         return render(request, 'main.html', {'QRCode': True, 'image':image})
